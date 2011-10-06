27097a3dde868af751ba3b3dc47ca980b4b9dcb4
Ronald <pietjebell31@hotmail.com>
2011-10-06 15:00:03 +0000

    Improved plugin
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/sitemap/trunk@22836 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- sitemap/plugins/imglossary.php
+++ sitemap/plugins/imglossary.php
@@ -13,16 +13,7 @@
 **/
 
 function b_sitemap_imglossary() {
-	$myts =& MyTextSanitizer::getInstance();
-	$result = icms::$xoopsDB -> query( 'SELECT categoryID, name FROM ' . icms::$xoopsDB -> prefix( 'imglossary_cats' ) . ' ORDER BY name' );
-	$ret = array() ;
-	while ( list( $id, $name ) = icms::$xoopsDB -> fetchRow( $result ) ) {
-		$ret['parent'][] = array(
-			'id' => $id,
-			'title' => $myts -> htmlSpecialChars( $name ),
-			'url' => 'category.php?categoryID=$id'
-		);
-	}
-	return $ret;
+	$block = sitemap_get_articles_map( icms::$xoopsDB -> prefix( 'imglossary_cats' ), 'categoryID', 'name', 'category.php?categoryID=', 'name' );
+	return $block;
 }
 ?>
