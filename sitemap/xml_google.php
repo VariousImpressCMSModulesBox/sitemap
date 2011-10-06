1bcb24423c2885326475908a9ac0d9bee26b51ce
Ronald <pietjebell31@hotmail.com>
2011-10-06 18:30:41 +0000

    Fixing Google sitemap file
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/sitemap/trunk@22837 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- sitemap/xml_google.php
+++ sitemap/xml_google.php
@@ -5,20 +5,20 @@
 *
 * File: xml_google.php
 *
-* @copyright	http://www.xoops.org/ The XOOPS Project
-* @copyright	XOOPS_copyrights.txt
-* @copyright	http://www.impresscms.org/ The ImpressCMS Project
-* @license		GNU General Public License (GPL)
-*				a copy of the GNU license is enclosed.
+* @copyright    http://www.xoops.org/ The XOOPS Project
+* @copyright    XOOPS_copyrights.txt
+* @copyright    http://www.impresscms.org/ The ImpressCMS Project
+* @license      GNU General Public License (GPL)
+*               a copy of the GNU license is enclosed.
 * ----------------------------------------------------------------------------------------------------------
-* @package		Sitemap 
-* @since		1.30
-* @author		chanoir
+* @package      Sitemap 
+* @since        1.30
+* @author       chanoir
 * ----------------------------------------------------------------------------------------------------------
-* 				Sitemap
-* @since		1.40
-* @author		McDonald
-* @version		$Id$
+*               Sitemap
+* @since        1.40
+* @author       McDonald
+* @version      $Id$
 */
 
 if ( ! defined( 'SITEMAP_ROOT_CONTROLLER_LOADED' ) ) {
@@ -40,44 +40,27 @@ if ( ! defined( 'SITEMAP_ROOT_CONTROLLER_LOADED' ) ) {
 	require '../../mainfile.php';
 }
 
-// icms::$module -> config['alltime_guest'] = true;
-
-// require_once ICMS_ROOT_PATH . '/class/template.php';
-
-// icms::$module -> config['with_lastmod'] = true;
+$sitemap_configs = $xoopsModuleConfig;
 
 if ( function_exists( 'mb_http_output' ) ) {
 	mb_http_output('pass');
 }
+
 header( 'Content-Type:text/xml; charset=utf-8' );
 
 include_once ICMS_ROOT_PATH . '/modules/sitemap/include/sitemap.php';
 
 $xoopsTpl = new icms_view_Tpl();
 
-// for All-time guest mode (backup uid & set as Guest)
-//if( is_object( $xoopsUser ) && ! empty( icms::$module -> config['alltime_guest'] ) ) {
-//	$backup_uid = $xoopsUser->getVar('uid') ;
-//	$xoopsUser = '' ;
-//	$xoopsUserIsAdmin = false ;
-//	$xoopsTpl->assign(array('xoops_isuser' => false, 'xoops_userid' => 0, 'xoops_uname' => '', 'xoops_isadmin' => false));
-//}
-
 $sitemap = sitemap_show();
-
-// for All-time guest mode (restore $xoopsUser*)
-//if( ! empty( $backup_uid ) && ! empty( icms::$module -> config['alltime_guest'] ) ) {
-//	$member_handler =& xoops_gethandler('member');
-//	$xoopsUser =& $member_handler->getUser( $backup_uid ) ;
-//	$xoopsUserIsAdmin = $xoopsUser->isAdmin();
-//}
 
 $xoopsTpl -> assign( 'lastmod', gmdate( 'Y-m-d\TH:i:s\Z' ) ); // TODO
 $xoopsTpl -> assign( 'sitemap', $sitemap );
-$xoopsTpl -> assign( 'show_subcategoris', icms::$module -> config['show_subcategoris'] );
+$xoopsTpl -> assign( 'show_subcategoris', $sitemap_configs['show_subcategoris'] );
 
 $xoopsTpl -> assign( 'this', array( 'mods' => icms::$module -> getVar( 'dirname' ), 'name' => icms::$module -> getVar( 'name' ) ) );
 
-if ( is_object( $icms_core_Logger ) ) $icms_core_Logger -> activated = false;
+icms::$logger -> disableLogger();
+
 $xoopsTpl -> display( 'db:sitemap_xml_google.html' );
 ?>
