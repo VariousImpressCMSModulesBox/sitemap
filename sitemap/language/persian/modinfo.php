e0b1196b68894bed92689d5a2939568c42fe7315
Ronald <pietjebell31@hotmail.com>
2011-10-29 11:53:11 +0000

    Added new defines to Persian language files
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/sitemap/trunk@22927 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- sitemap/language/persian/modinfo.php
+++ sitemap/language/persian/modinfo.php
@@ -31,4 +31,14 @@ define('_MI_SELECT_DEFAULT', 'Default');
 define('_MI_SELECT_SLICKMAP', 'Slickmap');
 define('_MI_WARNING_FINAL', "This module comes as is, without any guarantees whatsoever. Although this module is not beta, it is still under active development. This release can be used in a live website or a production environment, but its use is under your own responsibility, which means the author is not responsible.");
 define('_MI_WARNING_RC', "This module comes as is, without any guarantees whatsoever. This module is a <em>Release Candidate</em> and should <u>NOT</u> be used on a production web site. The module is still under active development and its use is under your own responsibility, which means the author is not responsible.");
+
+define('_MI_SITEMAP_MODULE_GO','Start module');
+
+define('_MI_CHANGEFREQ', 'Change frequency');
+define('_MI_CHANGEFREQ_DSC','How frequently the page is likely to change. This value provides general information to search engines and may not correlate exactly to how often they crawl the page. For more information: <a href="http://www.sitemaps.org/protocol.php#changefreqdef" target="_blank">Sitemap.org</a><br />This setting is for the Google sitemap file only!');
+define('_MI_CHANGEFREQ_HOURLY', 'hourly');
+define('_MI_CHANGEFREQ_DAILY', 'daily');
+define('_MI_CHANGEFREQ_WEEKLY', 'weekly');
+define('_MI_CHANGEFREQ_MONTHY', 'monthly');
+define('_MI_CHANGEFREQ_YEARLY', 'yearly');
 ?>
